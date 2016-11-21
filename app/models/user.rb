7660eac83f66fa2d066ca84148347e35207ef5d4
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-21 18:02:37 +0000

    removed user validations to allow for correct signup behaviour

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,6 @@ class User < ApplicationRecord
          authentication_keys: [:mobile_number]
   has_many :loans
 
-  validates :mobile_number, uniqueness: true, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-
   def email_required?
     false
   end
