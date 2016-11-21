24795f109b15f2a1aa8181e59c32394f0840285a
Julien Pageaud <julien.r.pageaud@gmail.com>
2016-11-21 17:18:46 +0000

    created validations in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :loans
+  validates :mobile_number, uniqueness: true, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+
 end
