e50281963e5ae51171bac452f99e2a2310390956
Will Tinney <willtinney1@gmail.com>
2016-11-21 18:11:32 +0000

    Generated loans controller and added resources for loans (new, create, edit, update, show)

---
+++ app/controllers/loans_controller.rb
@@ -0,0 +1,27 @@
+class LoansController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_loan
+  end
+
+  def strong_params
+  end
+end
