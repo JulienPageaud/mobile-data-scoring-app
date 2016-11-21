884ec94b25ffa82a9e73cd458bf9105b87038479
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-21 17:34:17 +0000

    changed e-mail required and changes methods for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
          :trackable, :validatable,
-         authentication_keys:[:mobile_number]
+         authentication_keys: [:mobile_number]
   has_many :loans
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
 
 end
