d203f56a1d45480154c55bdda3be8ab687035347
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-22 12:28:24 +0000

    added set_loan before action to controller

--- app/controllers/bank_users_controller.rb
+++ app/controllers/bank_users_controller.rb
@@ -3,7 +3,10 @@ class BankUsersController < ApplicationController
   # Authentication is on bank_user not user in this case
   skip_before_filter :authenticate_user!
 
+  before_action :set_loan, only: [:show, :edit, :update]
+
   def index
+    @loans = current_bank_user.bank.loans
   end
 
   def show
@@ -13,5 +16,11 @@ class BankUsersController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def set_loan
+    @loan = Loan.find(params[:id])
   end
 end
