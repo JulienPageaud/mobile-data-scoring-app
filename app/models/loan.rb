9e17ec0f52fc277ec4964604dcb6c991715348a8
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-22 16:34:34 +0000

    created next_payment_date, remaining_capital instance methods and a good_loans Class method for loans

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,6 +1,7 @@
 class Loan < ApplicationRecord
   belongs_to :bank
   belongs_to :user
+  has_many :payments
 
   monetize :requested_amount_cents
   monetize :proposed_amount_cents
@@ -12,4 +13,20 @@ class Loan < ApplicationRecord
   validates :description, presence: true, length: { minimum: 100,
     too_short: "You need to exceed %{count} characters in your description" }
 
+
+  def next_payment_date
+    payments.where('due_date < ?', DateTime.now).first
+  end
+
+  def self.good_loans
+    all.joins(:payments).where("payment.due_date <  #{DateTime.now}").where("payments.paid" => true)
+  end
+
+  def remaining_capital
+    payments_total = 0
+    payments.each do |payment|
+      payments_total += payment.amount_cents if payment.paid == true
+    end
+    return agreed_amount_cents - payments_total
+  end
 end
