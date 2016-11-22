b975574676af603306af590fe855b9e5ed0e2af0
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-22 11:18:09 +0000

    removed registerable from bank_user model (as we went to register them manually rather than allow a signup)

--- app/models/bank_user.rb
+++ app/models/bank_user.rb
@@ -1,7 +1,7 @@
 class BankUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable,
          authentication_keys: [:email]
 end
