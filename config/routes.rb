a450f1062c30ea31a2aaa553acf243ee517270d4
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-22 11:59:42 +0000

    added routes for bank's side of the app

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,14 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
+  # Routes for customer's side of the application
   resources :users, only: [ :show, :edit, :update ] do
     resources :loans, only: [ :new, :create, :edit, :update, :show ]
+  end
+
+  # Routes for the bank's side of the application
+  resources :bank_users, only:[:show] do
+    resources :loans, only: [:index, :show, :edit, :update], controller: :bank_users
+    resources :users, only: [:show]
   end
 end
