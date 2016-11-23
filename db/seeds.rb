a279873ed1b460e592ce04027e575e7c62b670ed
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-23 19:48:29 +0000

    added seeds and if statements to loans partial so that loans whose last payment is in the past (no next_payment) don't break the code

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,32 @@ description_ary = ["I really need to pay for a kidney transplant for my eldest s
                "I want to buy a large amount of cooking utensils for my kitchen as the restaurant is expanding",
                "I would like to open a clothes shop as the locals in the area are complaining and there is a good market",
                "I want to start my own business which is a belly dancer marketplace so that you can rent a belly dancer for your bar mitzvah"]
+start_date_outstanding_ary = [(DateTime.now), (DateTime.now - 1.month), (DateTime.now - 2.month), (DateTime.now - 3.month), (DateTime.now - 7.day)]
+start_date_repaid_ary = [(DateTime.now - 1.month), (DateTime.now - 2.month), (DateTime.now - 3.month), (DateTime.now - 20.day), (DateTime.now -3.day)]
+
 bank = Bank.last
 50.times do
-  user = User.create!(mobile_number: Faker::PhoneNumber.cell_phone, password: 'testtest')
+  user = User.create!(mobile_number: Faker::PhoneNumber.cell_phone, password: 'testtest', first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
   loan = user.loans.build(status: status_ary.sample, category: category_ary.sample, purpose: purpose_ary.sample,
                    description: description_ary.sample, interest_rate: 15, bank: bank,
                    requested_amount_cents: rand(1500000).round(-3))
+  loan.proposed_amount_cents = loan.requested_amount_cents
+  loan.agreed_amount_cents = loan.requested_amount_cents
+
+  case loan.status
+  when "Loan Outstanding"
+    loan.start_date = start_date_outstanding_ary.sample
+    loan.final_date = loan.start_date + loan.duration_months.month
+    loan.update_payments_to_agreed_amount
+  when "Loan Repaid"
+    loan.start_date = start_date_repaid_ary.sample
+    loan.final_date = loan.start_date + loan.duration_months.month
+  when "Application Accepted"
+    loan.create_payments_proposed
+  when "Application Declined"
+    loan.start_date = start_date_outstanding_ary.sample
+    loan.final_date = loan.start_date + loan.duration_months.month
+  end
   loan.save!
 end
 
