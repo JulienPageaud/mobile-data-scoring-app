da67fde567962172689a3a57c9b9b691297d56d3
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-23 15:29:19 +0000

    pundit stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_polic_scoped, only: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   private
 
