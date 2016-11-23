97e4ff0f6c0ab7c8089b253f00423b097298276b
Will Tinney <willtinney1@gmail.com>
2016-11-23 17:55:19 +0000

    Added user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+counter = 1
+
+6.times do
+  User.create( email: "user#{counter}@gmail.com", password: "123456",
+    mobile_number: "#{counter}", gender: "male", first_name: "Spider",
+    last_name: "Man", address: "8 Spidey Road", city: "New York",
+    postcode: "#{counter}", employment: "Superhero", details_completed: true)
+  counter += 1
+end
+
+Loan.create( user_id: 1, status: , category: "personal", purpose)
+
+
+User.all.each do |user|
+
