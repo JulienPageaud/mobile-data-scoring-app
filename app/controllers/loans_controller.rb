4be181d28785216abab1563d3fba2d66de75eea5
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-23 16:19:43 +0000

    added loan update logic for bank_user update action, added pundit set_uset method and a new bank_loan_params method

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -26,11 +26,33 @@ class LoansController < ApplicationController
   end
 
   def update
+    if params[loan: {:status}] == "Application Approved" || params[loan: {:status}] == "Application Declined"
+      authorize @loan
+      if @loan.update(loan_bank_params)
+      redirect_to bank_user_loans_path
+      else
+      render :show
+      end
+    elsif params[loan: {:agreed_amount_cents}] || params[loan: {:status}] == "Loan Outstanding"
+      # WILL/JULIEN YOU CAN PUT YOUR UPDATE CODE HERE
+    end
   end
 
   private
 
   def loan_params
     params.require(:loan).permit(:requested_amount_cents, :category, :purpose, :description)
+  end
+
+  def loan_bank_params
+    params.require(:loan).permit(:status, :proposed_amount_cents, :decline_reason, :final_date)
+  end
+
+  def pundit_user
+    if user_signed_in?
+      current_user
+    elsif bank_user_signed_in?
+      current_bank_user
+    end
   end
 end
