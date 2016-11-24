7ab45f34c5983d5c0b4a8e0aabe7143148b0d0f7
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-24 12:09:44 +0000

    added a few methods to loan model and added lots of comments for clarity

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -13,11 +13,48 @@ class Loan < ApplicationRecord
   validates :description, presence: true, length: { minimum: 50,
     too_short: "You need to exceed %{count} characters in your description" }
 
+  ## PAYMENT METHODS
 
+  # Retrieve the next payment which is due
   def next_payment
     payments.where('due_date > ?', DateTime.now).first
   end
 
+  # Retrieve the most recent payment which has passed it's due date
+  def most_recent_payment
+    payments.where('due_date < ?', DateTime.now).last
+  end
+
+  # Calculate the amount owed (sum of unpaid payments)
+  def amount_owed
+    amount = 0
+    payments.where('due_date < ?', DateTime.now).where(paid: false).each do |payment|
+      amount += payment.amount
+    end
+    return amount
+  end
+
+  # Calculate the remaining capital on the loan
+  def remaining_capital
+    payments_total = 0
+    payments.each do |payment|
+      payments_total += payment.amount if payment.paid == true
+    end
+    return agreed_amount - payments_total
+  end
+
+  # Calculate the total repaid capital
+  def total_capital_repaid
+    sum = 0
+    payments.each do |payment|
+      sum += payment.amount if payment.paid == true
+    end
+    sum.currency.to_s + ' ' + sum.to_s
+  end
+
+  ## LOAN FILTERS
+
+  # Finds loans which have no missed/delayed payments
   def self.good_loans
     result = (where(status: "Loan Outstanding").joins(:payments).where("payments.due_date < ?", DateTime.now).where(payments: {paid: true})).to_a
     new_loans = []
@@ -33,18 +70,12 @@ class Loan < ApplicationRecord
     end
   end
 
-  def remaining_capital
-    payments_total = 0
-    payments.each do |payment|
-      payments_total += payment.amount if payment.paid == true
-    end
-    return agreed_amount - payments_total
-  end
-
+  # Finds loans which have missed payments (due_date + 7 days)
   def self.missed_payment_loans
     where(status: "Loan Outstanding").joins(:payments).where('payments.due_date < ?', (DateTime.now - 7.day)).where(payments: { paid: false })
   end
 
+  # Finds loans which have delayed payments (less than 7 days since due date)
   def self.delayed_payment_loans
     where(status: "Loan Outstanding").joins(:payments).where('payments.due_date < ?',DateTime.now).where(payments: { paid: false })
   end
@@ -76,24 +107,22 @@ class Loan < ApplicationRecord
     end
   end
 
+
+  ## MONEY DISPLAYING METHODS
+  # display the agreed capital with currency + amount
   def display_capital
     agreed_amount.currency.to_s + ' ' + agreed_amount.to_s
   end
 
+  # display the proposed capital with currency + amount
   def display_proposed_capital
     proposed_amount.currency.to_s + ' ' + proposed_amount.to_s
   end
 
+  # display the requested capital with currency + amount
   def display_requested_capital
     requested_amount.currency.to_s + ' ' + requested_amount.to_s
   end
 
-  def total_capital_repaid
-    sum = 0
-    payments.each do |payment|
-      sum += payment.amount
-    end
-    sum.currency.to_s + ' ' + sum.to_s
-  end
 
 end
