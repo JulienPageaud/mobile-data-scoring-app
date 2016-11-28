5224344d7e3c8ef64dec71eac66267760e9a471e
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-28 16:56:19 +0000

    added a send_sms class method in Notification model. Created client class variable which is used to keep the code more DRY

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,6 +5,8 @@ class Notification < ApplicationRecord
 
   after_create :trigger_sms
 
+  @@client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+
   def trigger_sms
     if user.loans.last.status == "Application Accepted"
       sms_message = "Congratulations! Your loan has been accepted.
@@ -16,13 +18,19 @@ class Notification < ApplicationRecord
     send_text_notification(user.mobile_number, sms_message)
   end
 
+  def self.send_sms(mobile_number, body)
+    @@client.messages.create(
+      from: ENV['TWILIO_NUMBER'],
+      to: mobile_number,
+      body: body)
+  end
+
   private
 
   def send_text_notification(mobile_number, sms_message)
     twilio_number = ENV['TWILIO_NUMBER']
-    client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
 
-    client.messages.create(
+    @@client.messages.create(
       from: twilio_number,
       to: mobile_number,
       body: sms_message)
