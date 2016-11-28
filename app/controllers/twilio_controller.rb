51ac9d018464a3cb3a9e8c33508e856ea7522ab7
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-28 18:10:59 +0000

    fixed the confirm_loan action in twilio controller

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -4,18 +4,28 @@ class TwilioController < ApplicationController
   skip_before_action :authenticate_bank_user!
 
   def confirm_loan
-
     message_body = params["Body"]
     user = User.find_by_mobile_number(params["From"])
-    user = User.find_by_mobile_number("00447990740467")
 
     if message_body.downcase.include?("confirm")
+      user.loans.last.accept(accept_loan_params(user))
       user.confirm_loan
     elsif message_body.downcase.include?("decline")
+      user.loans.last.decline
       user.decline_loan
     else
       #user.error_text
     end
   end
 
+  private
+
+  def accept_loan_params(user)
+
+    loan = user.loans.last
+    return {  agreed_amount: loan.proposed_amount,
+              start_date: DateTime.now,
+              final_date: (DateTime.now + loan.duration_months.month)
+    }
+  end
 end
