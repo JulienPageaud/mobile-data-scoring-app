8a81490d51895dde68c8420c12de4baf55115449
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-29 09:42:42 +0000

    refactored notification model code

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -7,6 +7,14 @@ class Notification < ApplicationRecord
 
   @@client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
 
+  def self.send_sms(mobile_number, body)
+    @@client.messages.create(
+      from: ENV['TWILIO_NUMBER'],
+      to: mobile_number,
+      body: body)
+  end
+
+  # Triggered when a bank accepts/declines a loan application
   def trigger_sms
     @loan = user.loans.last
     if user.loans.last.status == "Application Accepted"
@@ -16,30 +24,19 @@ class Notification < ApplicationRecord
                      You can reply 'confirm' or 'decline' to this message, or log in
                      to the website to see the full details and confirm your loan.
                      "
-      sms_message.squish!
     elsif user.loans.last.status == "Application Declined"
       sms_message = "We are sorry to inform you that your loan application has
                      been declined for the following reason: #{@loan.decline_reason}.\
                      Please visit the Stride website for more details"
-      sms_message.squish!
     end
-    send_text_notification(user.mobile_number, sms_message)
-  end
-
-  def self.send_sms(mobile_number, body)
-    @@client.messages.create(
-      from: ENV['TWILIO_NUMBER'],
-      to: mobile_number,
-      body: body)
+    send_text_notification(user.mobile_number, sms_message.squish)
   end
 
   private
 
   def send_text_notification(mobile_number, sms_message)
-    twilio_number = ENV['TWILIO_NUMBER']
-
     @@client.messages.create(
-      from: twilio_number,
+      from: ENV['TWILIO_NUMBER'],
       to: mobile_number,
       body: sms_message)
   end
