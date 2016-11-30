ada8529c64be73d124e2a3b83adade065f864654
Will Tinney <willtinney1@gmail.com>
2016-11-30 12:37:25 +0000

    Added logic in Bank model methods

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,4 +1,51 @@
 class Bank < ApplicationRecord
   has_many :loans
+  has_many :users, through: :loans
+  has_many :payments, through: :loans
   has_many :bank_users
+
+  def net_balance_sheet
+    balance = 0
+    loans.where(status: "Loan Outstanding").each do |loan|
+      balance += loan.remaining_capital
+    end
+    loans.where(status: "Application Accepted").each do |loan|
+      balance -= loan.proposed_amount
+    end
+    return balance
+  end
+
+  def pending_response
+    loans.where(status: "Application Pending").count
+  end
+
+  def pending_repayment
+    loans.where(status: "Loan Outstanding").count
+  end
+
+  def total_users
+    users.count
+  end
+
+  def customers
+    users.joins(:loans).where("loans.status" => ["Application Accepted", "Loan Outstanding"]).count
+  end
+
+  def average_score_of_pending_customers
+    total_credit_score = 0
+    users.joins(:loans).where("loans.status" => "Application Pending").each do |user|
+      total_credit_score += user.credit_score.to_f
+    end
+
+    total_credit_score / users.joins(:loans).where("loans.status" => "Application Pending").count
+  end
+
+  def average_score_of_active_customers
+    total_score = 0
+      users.joins(:loans).where("loans.status" => ["Application Accepted", "Loan Outstanding"]).each do |user|
+        total_score += user.credit_score.to_f
+      end
+
+    total_score / customers
+  end
 end
