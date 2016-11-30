61150a121cad50a8706b236367bd82cef625f2aa
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-11-30 11:59:38 +0000

    improved seed files. changed radial progress bar colours

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,167 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-User.destroy_all
 Loan.destroy_all
+User.destroy_all
+BankUser.destroy_all
+Bank.destroy_all
+bank = Bank.create!(name: "FNB")
+BankUser.create!(email: "fnbemployee@fnb.com", password: "ilovemoney",
+  first_name: "Joel", last_name: "Banks", phone_number: '+27999785123',
+  bank: bank)
+BankUser.create!(email: "fnbemployee@gmail.com", password: "ilovemoney",
+  first_name: "Jan", last_name: "Mandela", phone_number: '+27999233233',
+  bank: bank)
+User.create!(mobile_number: 1234560, password: 123456, first_name: "Tom", last_name: "Cruise", )
 
-counter = 1
+cities_ary = ["Johannesburg", 'Cape Town', 'Durban', 'Pretoria', 'Port Elizabeth', 'Bloemfontein', 'East London']
 
-6.times do
-  User.create!(email: "user#{counter}@gmail.com", password: "123456",
-    mobile_number: "#{counter}", gender: "male", first_name: "Spider",
-    last_name: "Man", address: "8 Spidey Road", city: "New York",
-    postcode: "#{counter}", employment: "Superhero", details_completed: true)
-  counter += 1
+status_ary = ["Application Pending", "Application Accepted", "Loan Outstanding", "Application Declined", "Loan Repaid"]
+category_ary = ["Personal", "Business"]
+purpose_ary = ["Medical Expenses", "Start a business", "Open a shop", "Coding School", "Beer Money"]
+description_ary = ["I need to pay for a organ transplant for my son",
+  "I want to buy a large amount of cooking utensils for my kitchen as the restaurant is expanding",
+  "I would like to open a clothes shop as there are none in this area",
+  "I want to start my own tech business and I need the money to launch my product",
+  "I want to learn how to code Ruby on Rails applications at Le Wagon",
+  "I'm really thirsty so I want to buy lots of beer"]
+decline_reason_ary = ["Credit score too low",
+  "Insufficient mobile data", "Criminal record"]
+
+start_date_outstanding_ary = [(DateTime.now - 3.day), (DateTime.now - 1.month),
+  (DateTime.now - 2.month), (DateTime.now - 15.day),
+  (DateTime.now - 8.day - 1.month)]
+start_date_repaid_ary = [(DateTime.now - 1.month), (DateTime.now - 2.month),
+  (DateTime.now - 3.month), (DateTime.now - 20.day),
+  (DateTime.now -3.day)]
+
+# Pending Applications
+30.times do
+  mobile_string = '7' + ('%010d' % rand(10 ** 9)).to_s
+  user = User.create!(mobile_number: '+27' + mobile_string,
+    password: 'testtest', first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name, city: cities_ary.sample,
+    date_of_birth: Faker::Date.between(18.years.ago, 80.years.ago), credit_score: (rand(20) + 80).fdiv(100).round(2))
+
+  loan = user.loans.build(category: category_ary.sample,
+    status: "Application Pending", purpose: purpose_ary.sample,
+    description: description_ary.sample, interest_rate: 15,
+    bank: bank, requested_amount: rand(100..15000).round(-2))
+  loan.save!
 end
 
-Loan.create!(user_id: 2, status: "Application Pending", category: "personal", purpose: "Medical Expenses",
-  description: "I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... ",
-  interest_rate: 15, start_date: nil, final_date: nil, requested_amount_cents: 50000,
-  proposed_amount_cents: 0, agreed_amount_cents: 0, duration_months: 3)
-Loan.create!(user_id: 3, status: "Application Accepted", category: "personal", purpose: "Medical Expenses",
-  description: "I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... ",
-  interest_rate: 15, start_date: nil, final_date: nil, requested_amount_cents: 50000,
-  proposed_amount_cents: 50000, agreed_amount_cents: 0, duration_months: 3)
-Loan.create!(user_id: 4, status: "Loan Outstanding", category: "personal", purpose: "Medical Expenses",
-  description: "I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... ",
-  interest_rate: 15, start_date: nil, final_date: nil, requested_amount_cents: 50000,
-  proposed_amount_cents: 50000, agreed_amount_cents: 50000, duration_months: 3)
-Loan.create!(user_id: 5, status: "Application Declined", category: "personal", purpose: "Medical Expenses",
-  description: "I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... ",
-  interest_rate: 15, start_date: nil, final_date: nil, requested_amount_cents: 50000,
-  proposed_amount_cents: 0, agreed_amount_cents: 0, duration_months: 3)
-Loan.create!(user_id: 6, status: "Loan Repaid", category: "personal", purpose: "Medical Expenses",
-  description: "I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... I need this loan for spider things... ",
-  interest_rate: 15, start_date: nil, final_date: nil, requested_amount_cents: 50000,
-  proposed_amount_cents: 50000, agreed_amount_cents: 50000, duration_months: 3)
+# Accepted Application
+15.times do
+  mobile_string = '7' + ('%010d' % rand(10 ** 9)).to_s
+  user = User.create!(mobile_number: '+27' + mobile_string,
+    password: 'testtest', first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name, city: cities_ary.sample,
+    date_of_birth: Faker::Date.between(18.years.ago, 80.years.ago), credit_score: (rand(10) + 90).fdiv(100).round(2))
 
+  loan = user.loans.build(category: category_ary.sample,
+    status: "Application Accepted", purpose: purpose_ary.sample,
+    description: description_ary.sample, interest_rate: 15,
+    bank: bank, requested_amount: rand(100..15000).round(-2))
+  loan.update!(proposed_amount: loan.requested_amount)
+  loan.create_payments_proposed
+end
+
+# Good Book Loans
+30.times do
+  mobile_string = '7' + ('%010d' % rand(10 ** 9)).to_s
+  user = User.create!(mobile_number: '+27' + mobile_string,
+    password: 'testtest', first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name, city: cities_ary.sample,
+    date_of_birth: Faker::Date.between(18.years.ago, 80.years.ago), credit_score: (rand(10) + 90).fdiv(100).round(2))
+
+  loan = user.loans.build(category: category_ary.sample,
+    status: "Loan Outstanding", purpose: purpose_ary.sample,
+    description: description_ary.sample, interest_rate: 15,
+    bank: bank, requested_amount: rand(100..15000).round(-2))
+  loan.update!(proposed_amount: loan.requested_amount, agreed_amount: loan.requested_amount,
+    start_date: DateTime.now - (rand(1..50).round.day))
+  loan.update!(final_date: (loan.start_date + loan.duration_months.month))
+  loan.update_payments_to_agreed_amount
+  loan.payments.each do |payment|
+    payment.update!(paid: true) if payment.due_date < DateTime.now
+  end
+end
+
+# Missed Payment Loans
+3.times do
+    mobile_string = '7' + ('%010d' % rand(10 ** 9)).to_s
+  user = User.create!(mobile_number: '+27' + mobile_string,
+    password: 'testtest', first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name, city: cities_ary.sample,
+    date_of_birth: Faker::Date.between(18.years.ago, 80.years.ago), credit_score: (rand(10) + 80).fdiv(100).round(2))
+
+  loan = user.loans.build(category: category_ary.sample,
+    status: "Loan Outstanding", purpose: purpose_ary.sample,
+    description: description_ary.sample, interest_rate: 15,
+    bank: bank, requested_amount: rand(100..15000).round(-2))
+  loan.update!(proposed_amount: loan.requested_amount, agreed_amount: loan.requested_amount,
+    start_date: DateTime.now - (rand(1..2).month + (rand(8..50).round.day)))
+  loan.update!(final_date: (loan.start_date + loan.duration_months.month))
+  loan.update_payments_to_agreed_amount
+  loan.payments.each do |payment|
+    payment.update!(paid: true) if payment.due_date < DateTime.now
+  end
+  loan.most_recent_payment.update!(paid:false)
+end
+
+# Delayed Payment Loans
+2.times do
+  mobile_string = '7' + ('%010d' % rand(10 ** 9)).to_s
+  user = User.create!(mobile_number: '+27' + mobile_string,
+    password: 'testtest', first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name, city: cities_ary.sample,
+    date_of_birth: Faker::Date.between(18.years.ago, 80.years.ago), credit_score: (rand(10) + 80).fdiv(100).round(2))
+
+  loan = user.loans.build(category: category_ary.sample,
+    status: "Loan Outstanding", purpose: purpose_ary.sample,
+    description: description_ary.sample, interest_rate: 15,
+    bank: bank, requested_amount: rand(100..15000).round(-2))
+  loan.update!(proposed_amount: loan.requested_amount, agreed_amount: loan.requested_amount,
+    start_date: DateTime.now - (1.month + (rand(1..7).round.day)))
+  loan.update!(final_date: (loan.start_date + loan.duration_months.month))
+  loan.update_payments_to_agreed_amount
+  loan.payments.each do |payment|
+    payment.update!(paid: true) if payment.due_date < DateTime.now
+  end
+  loan.most_recent_payment.update(paid:false)
+end
+
+# Repaid Payment Loans
+10.times do
+  mobile_string = '7' + ('%010d' % rand(10 ** 9)).to_s
+  user = User.create!(mobile_number: '+27' + mobile_string,
+    password: 'testtest', first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name, city: cities_ary.sample,
+    date_of_birth: Faker::Date.between(18.years.ago, 80.years.ago), credit_score: (rand(5) + 92).fdiv(100).round(2))
+
+  loan = user.loans.build(category: category_ary.sample,
+    status: "Loan Repaid", purpose: purpose_ary.sample,
+    description: description_ary.sample, interest_rate: 15,
+    bank: bank, requested_amount: rand(100..15000).round(-2))
+  loan.update!(proposed_amount: loan.requested_amount, agreed_amount: loan.requested_amount,
+    start_date: DateTime.now - (rand(1..50).round.day))
+  loan.update!(final_date: (loan.start_date + loan.duration_months.month))
+  loan.update_payments_to_agreed_amount
+  loan.payments.each do |payment|
+    payment.update!(paid: true)
+  end
+end
+
+
+# Declined Payment Loans
+10.times do
+  mobile_string = '7' + ('%010d' % rand(10 ** 9)).to_s
+  user = User.create!(mobile_number: '+27' + mobile_string,
+    password: 'testtest', first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name, city: cities_ary.sample,
+    date_of_birth: Faker::Date.between(18.years.ago, 80.years.ago), credit_score: (rand(10) + 80).fdiv(100).round(2))
+
+  loan = user.loans.build(category: category_ary.sample,
+    status: "Application Declined", purpose: purpose_ary.sample,
+    description: description_ary.sample, interest_rate: 15,
+    bank: bank, requested_amount: rand(100..15000).round(-2),
+    decline_reason: decline_reason_ary.sample)
+  loan.update!(created_at: DateTime.now - rand(3..10).day, final_date: DateTime.now - rand(0..2).day)
+end
