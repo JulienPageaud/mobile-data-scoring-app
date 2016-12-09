e3b63bdf2a5fcee45ae83261fdaa457c77a95e34
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-12-09 09:27:00 +0000

    reverted to postmark smtp settings

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: "mail.gandi.net",
-  port: 587,
-  domain: 'strideworld.com',
-  user_name: ENV['GANDI_LOGIN'],
-  password: ENV['GANDI_PASSWORD'],
-  authentication: :login,
+  address: ENV['POSTMARK_SMTP_SERVER'],
+  port: '25',
+  domain: 'heroku.com',
+  user_name: ENV['POSTMARK_API_KEY'],
+  password: ENV['POSTMARK_API_TOKEN'],
+  authentication: :cram_md5,
   enable_starttls_auto: true
 }
