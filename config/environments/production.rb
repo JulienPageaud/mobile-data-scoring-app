850ee1df52a97cfbb9f17ba11d06def46b16f276
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-12-09 09:19:01 +0000

    actionmailer default url test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
 
   # Email configuration
-  config.action_mailer.default_url_options = { host: "score-app.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "strideworld.com" }
   config.action_mailer.delivery_method = :smtp
 
   # Settings specified here will take precedence over those in config/application.rb.
