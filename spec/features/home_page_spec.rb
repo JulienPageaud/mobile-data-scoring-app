535df0d01569c8de4000bca6ee38923e5a8cea5d
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-12-14 15:15:13 +0000

    configured test so that database is clean before tests. home_page specs improved

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 
 feature 'Home Page' do
-  scenario 'Visitor can load the home page and sign up' do
-    visit '/'
+  before do
+    visit root_path
+  end
+
+  it 'can be reached' do
+    expect(page.status_code).to eq(200)
+  end
+
+  scenario 'visitor can load the home page and sign up' do
     user_can_complete_mobile_number_and_click_apply
     user_can_see_modal_and_complete_sign_up
     user_is_redirected_to_complete_their_details
-    save_and_open_screenshot
   end
 
+  scenario 'registered user can sign in'
+
+  scenario 'failed sign up returns user to sign up form' do
+    user_can_complete_mobile_number_and_click_apply
+    # User forgets to enter password/confirmation
+    click_on('Sign up')
+    expect(page).to have_css('.modal-dialog')
+  end
 
   private
 
@@ -30,6 +44,7 @@ feature 'Home Page' do
   end
 
   def user_is_redirected_to_complete_their_details
-    expect(page)
+    expect(page.status_code).to eq(200)
+    expect(current_url.include?('/edit')).to eql(true)
   end
 end
