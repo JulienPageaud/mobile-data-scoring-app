0ad7892c2df02dcbff39a61bc11a7adf7b0a8ce2
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-12-14 13:51:42 +0000

    created home_page specs

---
+++ spec/features/home_page_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'Home Page' do
+  scenario 'Visitor can load the home page and sign up' do
+    load_page '/'
+    user_can_complete_mobile_number_and_click_apply
+    user_can_see_modal_and_complete_sign_up
+
+  end
+
+
+  private
+
+  def user_can_complete_mobile_number_and_click_apply
+    expect(page).to have_field('user[mobile_number]',  type: 'text')
+    fill_in 'mobile-input', with: '712341234'
+    expect(page).to have_selector(:link_or_button, "Apply for a loan")
+    click_on('submit')
+  end
+
+  def user_can_see_modal_and_complete_sign_up
+    expect(page).to have_css('modal-dialog')
+    expect(page).to have_field('user_password')
+    expect(page).to have_field('user_password_confirmation')
+    expect(page).to have_selector(:link_or_button, 'Sign up')
+    fill_in 'user_password', with: '123123'
+    fill_in 'user_password_confirmation', with: '123123'
+    click_on('Sign up')
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,8 @@ require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'support/factory_girl'
 require 'capybara/rspec'
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
