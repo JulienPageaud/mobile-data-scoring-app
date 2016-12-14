8f6c885905dde0310c2993add650f78fa83e194c
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-12-14 14:02:30 +0000

    home page spec tests user sign up

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'Home Page' do
   scenario 'Visitor can load the home page and sign up' do
-    load_page '/'
+    visit '/'
     user_can_complete_mobile_number_and_click_apply
     user_can_see_modal_and_complete_sign_up
 
@@ -15,11 +15,11 @@ feature 'Home Page' do
     expect(page).to have_field('user[mobile_number]',  type: 'text')
     fill_in 'mobile-input', with: '712341234'
     expect(page).to have_selector(:link_or_button, "Apply for a loan")
-    click_on('submit')
+    click_on('Apply for a loan')
   end
 
   def user_can_see_modal_and_complete_sign_up
-    expect(page).to have_css('modal-dialog')
+    expect(page).to have_css('.modal-dialog')
     expect(page).to have_field('user_password')
     expect(page).to have_field('user_password_confirmation')
     expect(page).to have_selector(:link_or_button, 'Sign up')
