0f171acd3381dcdf0322f0846e894597f4a281cc
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-12-17 13:43:14 +0000

    increased Capybara timeout to see if Travis build will pass

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,7 +15,7 @@ Capybara.javascript_driver = :poltergeist
 
 options = { js_errors: false }
 Capybara.register_driver :poltergeist do |app|
-    Capybara::Poltergeist::Driver.new(app, options)
+    Capybara::Poltergeist::Driver.new(app, timeout: 20)
 end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
