4d924ebc4928d21f92276f22746ce577caf2e209
Christos Dimitroulas <c.dimitroulas@gmail.com>
2016-12-21 18:53:20 +0000

    first bank user spec

--- spec/features/bank_dashboards_spec.rb
+++ spec/features/bank_dashboards_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 feature "BankDashboards" do
   let(:bank_user) { FactoryGirl.create(:bank_user)}
 
-  before do
-    login_as(bank_user, :scope => :bank_user)
-  end
+  # before do
+  #   login_as(bank_user, :scope => :bank_user)
+  # end
 
   scenario "bank user can log in to the dashboard", :skip_before do
-    logout(bank_user)
     visit '/bank_users/sign_in'
-    save_and_open_screenshot
     expect(page).to have_field('bank_user[email]')
     expect(page).to have_field('bank_user[password]')
 
     fill_in 'bank_user[email]', with: "fnbemployee@firstnational.com"
-    fill_in 'bank_user[password]', with: "fnbemployee@ilovemoney.com"
+    fill_in 'bank_user[password]', with: "ilovemoney"
     click_on 'Log in'
+    save_and_open_screenshot
     expect(current_url.include?('/loans')).to eql(true)
   end
+
 
   scenario "bank user can accept a loan application"
 
