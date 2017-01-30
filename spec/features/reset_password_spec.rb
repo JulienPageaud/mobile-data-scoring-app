1c00e9b759ad3d82fec82ebc8913fa653adabdb8
Cyrille Labesse <cyrille@inomads.hk>
2017-01-30 17:09:02 +0800

    Added basic specs and set devise paranoid ON

---
+++ spec/features/reset_password_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature 'Password reset' do
+  before do
+    ActionMailer::Base.deliveries = []
+    visit root_path
+    page.find('.glyphicon-user').click
+    click_on('Sign in')
+    click_on('Forgot your password?')
+    expect(page).to have_content('Email or mobile number')
+  end
+
+  it 'can be reached' do
+    expect(page.status_code).to eq(200)
+  end
+
+  scenario 'visitor cannot reset his password if account is inexistant' do
+    user_can_enter_reset_information_and_submit('unknown@email.com')
+    expect(ActionMailer::Base.deliveries.count).to eq(0)
+  end
+
+  scenario 'visitor can reset his password through email' do
+    user = FactoryGirl.create(:user)
+    user_can_enter_reset_information_and_submit(user.email)
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
+  end
+
+  private
+
+  def user_can_enter_reset_information_and_submit(email)
+    expect(current_path).to eq('/users/password/new')
+    fill_in 'user[email]', with: email
+    click_on "Send me reset password instructions"
+    expect(current_path).to eq('/users/sign_in')
+    expect(page).to have_content('If your email address exists in our database, you will receive an email with instructions for how to confirm your email address in a few minutes.')
+  end
+end
